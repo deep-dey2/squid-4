a90bc1f5d1f91524811473404311aa94297dd358
deep-dey2 <ddey676567@gmail.com>
2025-06-29 11:10:44 +0600

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  content: ["./*.html"],
+  theme: {
+    extend: {
+      colors: {
+        primary: '#1E40AF',
+        secondary: '#F59E0B',
+      },
+      fontFamily: {
+        sans: ['Poppins', 'sans-serif'],
+        mono: ['Fira Code', 'monospace'],
+      },
+    },
+  },
+  plugins: [],
+}
